8571d8fc92cee3f4bb285ce58495e41b3342d7fb
guan <kuniyasu.guan@gmail.com>
2017-06-29 18:44:28 +0900

    set default fong

--- init.d/init-fonts.el
+++ init.d/init-fonts.el
@@ -1,18 +1,17 @@
 
 ;; Ricty Fonts
 ;; https://github.com/yascentur/Ricty
-(set-face-attribute 'default nil
-                    :family "Ricty Diminished Discord"
-                    :height 120)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0212
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'katakana-jisx0201
-                  (cons "Ricty Diminished Discord" "iso10646-1"))
-
+;; (set-face-attribute 'default nil
+;;                     :family "Ricty Diminished Discord"
+;;                     :height 120)
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0208
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0212
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'katakana-jisx0201
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
 
 (provide 'init-fonts)
